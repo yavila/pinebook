9bc95fd91c97c915d7997e719c4d3cd6a91c9b9e
Yamilex Avila-Stanley <yavila.stanley@gmail.com>
2018-06-05 21:45:05 -0700

    Add basic auth
    Complete sign-in page
    
    Use SSL and Puma webserver in prod

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
-  def new
+  def show
+  	@user = User.find(params[:id])
+  	# debugger
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
   get 'users/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/signup', to: 'users#new'
+  resources :users
 end
